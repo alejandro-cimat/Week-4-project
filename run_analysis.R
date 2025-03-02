458ad0d5162c3f378f209f8021892ba300c02604
alejandro-cimat <guae@cimat.mx>
2025-03-02 13:09:03 -0600

    Update run_analysis.R: Updatinf file paths

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 # 0) Loading data sets and libraries
 library(dplyr)
 
-X_train <- read.table("X_train.txt")
-y_train <- read.table("y_train.txt")
-X_test <- read.table("X_test.txt")
-y_test <- read.table("y_test.txt")
-subject_train <- read.table("subject_train.txt")
-subject_test <- read.table("subject_test.txt")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
 y_train <- bind_cols(y_train, subject_train)
 y_test <- bind_cols(y_test, subject_test)
@@ -23,13 +23,13 @@ Means <- X %>% summarise( across(V1:V561, mean) )
 SDs <- X %>% summarise( across(V1:V561, sd) )
 
 # 3) Uses descriptive activity names to name the activities in the data set
-labels <- read.table("activity_labels.txt")
+labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 for (i in 1:6)
     y[y$Activity == i,1] <- labels$V2[i]
 
 # 4) Appropriately labels the data set with descriptive variable names. 
-features <- read.table("features.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
 colnames(X) <- features$V2
 
 # 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
